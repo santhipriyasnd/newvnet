43b83a4354f9d461447ec5b98be30ef48717c1dd
santhipriyasnd <78582900+santhipriyasnd@users.noreply.github.com>
2021-02-05 18:37:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   version         = "=2.4.0"
   subscription_id = "395b2b6c-8fb6-4cc1-83e4-39455be6b102"
-  client_id       = "df4415d0-e7a2-4334-9d5c-fa45d42eaeee"
-  client_secret   = "8~pdhCQUZ~yMTP_M-hCK1imb~AO22L1.wn"
+  client_id       = "72b2db2e-be38-456a-90ba-6f56877fc782"
+  client_secret   = "uLhyQ-cxN5Z6GgM_U4-C4.8X6xEGn_c-6~"
   tenant_id       = "90c3f360-0a02-49e4-b70c-4ebb69378edf"
   features {}
 }
