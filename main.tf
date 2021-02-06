1a134fa6cbbd779fc255a3893266beb378f9f428
santhipriyasnd <78582900+santhipriyasnd@users.noreply.github.com>
2021-02-06 16:29:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 resource "azurerm_resource_group" "main" {
-  name     = "pra"
+  name     = "pra1"
   location = "Westus"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "pravnet1"
+  name                = "pravnet12"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "main" {
-  name                 = "subnet2"
+  name                 = "subnet23"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "pnic1"
+  name                = "pnic12"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
